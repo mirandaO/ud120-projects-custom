64d6baf5909cb4f7eb6043e0ecfa9e087387f23b
mirandaO <miranda.oscar.mo@gmail.com>
2025-03-13 01:49:11 -0600

    Resolve Naive Bayes exercide and fix setup issues for python 2.7

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,42 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+t0 = time()
+classifier.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+accuracy = classifier.score(features_test, labels_test)
+print(accuracy)
 
+
+##This number is the one that solves the excersice after submitting it in the Course Quiz.
+truncated_accuracy = int(accuracy*10000) / 10000.0
+if (truncated_accuracy) == 0.9732:
+    print("Exercise solved")
+
+print("####################This is for understanding the features_train contents##########################")
+print(str(features_train.shape) + "Where: (n, k) -> n rows, k columns")
+print("Data type of elements: " + str(features_train.dtype))
+print("Number of dimensions " + str(features_train.ndim))
+print("Total number of elements: " + str(features_train.size))
+print("features_train: \n" + str(features_train))
+print("features_train[10000, 3000]: " + str(features_train[10000, 3784]))
+print("####################END##########################")
+
+
+print("####################This is for understanding the features_test contents##########################")
+print(str(features_test.shape) + "Where: (n, k) -> n rows, k columns")
+print("Data type of elements: " + str(features_test.dtype))
+print("Number of dimensions " + str(features_test.ndim))
+print("Total number of elements: " + str(features_test.size))
+print("features_train: \n" + str(features_test))
+print("features_train[1000, 3784]: " + str(features_test[1000, 3784]))
+print("####################END##########################")
+
+t0 = time()
+classifier.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
 
 #########################################################
 
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -45,7 +45,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### text vectorization--go from strings to lists of numbers
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
+                                stop_words='english')
     features_train_transformed = vectorizer.fit_transform(features_train)
     features_test_transformed  = vectorizer.transform(features_test)
 
